a57d1f4e001c60f2235c4b48d47969c5b9a2a794
Tusshar Lingagiri <tussharlingagiri@gmail.com>
2025-12-10 13:03:07 +0000

    Fix App.js with clean version - add all missing imports

--- src/App_broken.js
+++ src/App_broken.js
@@ -1,14 +1,5 @@
-import React, { useState, useEffect } from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import HeroVideo from "./components/HeroVideo";
-import Services from "./components/Services";
-import Testimonials from "./components/Testimonials";
-import ContactForm from "./components/ContactForm";
-import BottomBar from "./components/BottomBar";
-import StickyCTA from "./components/StickyCTA";
-import "./App.css";
 
+// ErrorBoundary class (only one, after all imports)
 class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props);
